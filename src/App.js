acac965e80a4bddb5168be4e9d885d487ee4970a
nkneeraj <nkneerj641@gmail.com>
2020-11-27 09:26:50 +0530

    [NK]Refactored: HelloWorld using Interpolation Data Binding.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import App2 from './App2';
 import React from "react"
 
+// class App extends React.Component{
+//   render(){
+//     return <div><App2></App2>
+//     <App2></App2>
+//     <App2></App2></div>
+//   }
+// }
+
 class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      title:'hello from Neeraj'
+    }
+   }
   render(){
-    return <div><App2></App2>
-    <App2></App2>
-    <App2></App2></div>
+    return <div><h1>{this.state.title}</h1></div>
   }
 }
-
-
 export default App;
