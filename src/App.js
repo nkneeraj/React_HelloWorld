99ab95d631f0deb45997d7aea29991e00e224536
nkneeraj <nkneerj641@gmail.com>
2020-11-27 09:18:44 +0530

    [NK]Added: App.js HelloWorld text.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import App2 from './App2';
+import React from "react"
+
+class App extends React.Component{
+  render(){
+    return <div><App2></App2>
+    <App2></App2>
+    <App2></App2></div>
+  }
+}
+
+
+export default App;
---
+++ src/App2.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react"
+
+class App extends React.Component{
+  render(){
+    return <div><h1>Hello world1</h1></div>
+  }
+}
+
+
+export default App;
